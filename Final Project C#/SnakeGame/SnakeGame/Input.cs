8ee046f9c7b1650d14fc693e11f1b6b79e611c3d
ABDULLAH-AL-HUSSAIN JEELAN <67293300+Abdullah189-D@users.noreply.github.com>
2021-02-07 01:23:03 +0600

    UPLOADED

---
+++ Final Project C#/SnakeGame/SnakeGame/Input.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    class Input
+    {
+        private static Hashtable KeyTable = new Hashtable();
+        
+        public static bool KeyPress(Keys key)
+        {
+            
+            if (KeyTable[key] == null)
+            {
+                
+                return false;
+            }
+            
+            return (bool)KeyTable[key];
+        }
+        public static void changeState(Keys key, bool state)
+        {
+            
+
+            KeyTable[key] = state;
+        }
+
+    }
+}
+
